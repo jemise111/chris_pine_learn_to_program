bf356ba4de59f3b178be9d44faf9f4a0b63a74e4
Jesse Sessler <jesse.sessler@gmail.com>
2013-11-19 15:21:47 -0500

    added chapter 6

---
+++ chapter_6/angry_boss.rb
@@ -0,0 +1,6 @@
+# Asks user what they want, then yells it back at them and fires them
+
+puts 'What do you want?'
+user_wants = gets.chomp
+
+puts 'WHADDAYA MEAN "' + user_wants.upcase + '"?!? YOU\'RE FIRED!!'
