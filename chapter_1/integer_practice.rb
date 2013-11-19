adf5b9e11187a985b14d4f01364b55c522c7d8a6
Jesse Sessler <jesse.sessler@gmail.com>
2013-11-19 14:20:30 -0500

    first commit

---
+++ chapter_1/integer_practice.rb
@@ -0,0 +1,15 @@
+# Chapter 1
+#------------
+# Solutions to the four practice questions
+
+# Hours in a year:
+puts 365.25 * 24
+
+# Minutes in a decade:
+puts 10 * 365.25 * 24 * 60 
+
+# My age in seconds:
+puts ((23 * 365.25 + 10) * 24 + 14) * 60 * 60
+
+# Author's age, in years, at time of writing:
+puts 1025000000 / 60 / 60 / 24 / 365
